db39fe967d6e83db5857034a6ccbd235613d2ec2
J. Dylan White <jdwhite5@ncsu.edu>
2022-04-02 21:48:32 -0400

    first commit

---
+++ custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(visual-fill-column org-bullets zenburn-theme which-key vdiff-magit use-package unobtrusive-magit-theme rainbow-delimiters nord-theme ivy-rich helpful general flycheck evil-collection elpy doom-themes doom-modeline counsel-projectile command-log-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-drawer ((t (:foreground "light slate gray" :weight normal)))))
